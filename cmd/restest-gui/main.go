5ee573e8318f142b84ad6ffd84cc902896e8e31b
lfree <xh.dreamlover@gmail.com>
2018-03-17 17:41:53 +0700

    go static server test ok

--- cmd/restest-gui/main.go
+++ cmd/restest-gui/main.go
@@ -1,4 +1,12 @@
 package main
 
+import "net/http"
+
 func main() {
+	// static server
+	staticS := &http.Server{
+		Addr:    ":8080",
+		Handler: http.FileServer(http.Dir("/Users/lfree/Works/github.com/src/github.com/AYJiaYou/restest-gui/web/dist")),
+	}
+	staticS.ListenAndServe()
 }
